a0a6b1568dd8b7f6cbac6a899225c12e9e6d7041
bluth653426595 <ycl_pj@163.com>
2013-07-16 17:22:34 +0800

    update

---
+++ test_log.c
@@ -0,0 +1,17 @@
+top:24641): DEBUG: object_path : /org/freedesktop/Accounts/User1000
+             ** (desktop:24641): DEBUG: picture_index : 0
+                                 [Thread 0x7fff90e7b700 (LWP 24657) exited]
+                                 [Thread 0x7fffa0c07700 (LWP 24653) exited]
+                                 [Thread 0x7fff91e7d700 (LWP 24655) exited]
+                                 ** (desktop:24641): DEBUG: draw background:
+                                                     [Thread 0x7fff9167c700 (LWP 24656) exited]
+
+                                                     (desktop:24641): GLib-GIO-CRITICAL **: g_file_info_get_attribute_uint32: assertion `G_IS_FILE_INFO (info)' failed
+
+                                                                      (desktop:24641): GLib-GObject-CRITICAL **: g_object_unref: assertion `G_IS_OBJECT (object)' failed
+                                                                                       ** (desktop:24641): DEBUG: hide mode: 0
+                                                                                                           ** (desktop:24641): DEBUG: {"x":0, "y":0, "width":1440, "height":840}
+                                                                                                           [New Thread 0x7fff9167c700 (LWP 24665)]
+                                                                                                           [New Thread 0x7fff91e7d700 (LWP 24666)]
+                                                                                                           [Thread 0x7fff91e7d700 (LWP 24666) exited]
+
